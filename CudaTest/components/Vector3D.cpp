6b1d28f34140626b8e0ea8383c15a37ead4636c6
Ryan Enns <ryanennns@gmail.com>
2024-03-13 08:10:25 -0400

    a lot of stuff

--- CudaTest/components/Vector3D.cpp
+++ CudaTest/components/Vector3D.cpp
@@ -1,78 +1,73 @@
+#include "./headers/Vector3D.h"
 #include <cmath>
 #include <stdio.h>
 
-class Vector3D
-{
-public:
-    double x;
-    double y;
-    double z;
-
-    Vector3D()
+namespace components {
+    Vector3D::Vector3D()
     {
         this->x = 0.0;
         this->y = 0.0;
         this->z = 0.0;
     }
 
-    Vector3D(Vector3D* v)
+    Vector3D::Vector3D(Vector3D* v)
     {
         this->x = v->x;
         this->y = v->y;
         this->z = v->z;
     }
 
-    Vector3D(double x, double y, double z)
+    Vector3D::Vector3D(double x, double y, double z)
     {
         this->x = x;
         this->y = y;
         this->z = z;
     }
 
-    double magnitude()
+    double Vector3D::magnitude()
     {
-        return std::sqrt(x*x + y*y + z*z);
+        return std::sqrt(x * x + y * y + z * z);
     }
 
-    Vector3D* normalized()
+    Vector3D Vector3D::normalized()
     {
         double magnitude = this->magnitude();
 
-        return new Vector3D(
+        return Vector3D(
             this->x / magnitude,
             this->y / magnitude,
             this->z / magnitude
         );
     }
 
-    Vector3D* add(Vector3D* v)
+    Vector3D Vector3D::add(Vector3D* v)
     {
-        this->x = this->x + v->x;
-        this->y = this->y + v->y;
-        this->z = this->z + v->z;
-
-        return this;
+        return Vector3D(
+            this->x + v->x,
+            this->y + v->y,
+            this->z + v->z
+        );
     }
 
-    Vector3D* subtract(Vector3D* v)
+    Vector3D Vector3D::subtract(Vector3D* v)
     {
-        this->x = this->x - v->x;
-        this->y = this->y - v->y;
-        this->z = this->z - v->z;
-
-        return this;
+        return Vector3D(
+            this->x - v->x,
+            this->y - v->y,
+            this->z - v->z
+        );
     }
 
-    Vector3D* multiply(double scalar)
+    Vector3D Vector3D::multiply(double scalar)
     {
-        this->x = this->x * scalar; 
-        this->y = this->y * scalar; 
-        this->z = this->z * scalar; 
-    
-        return this;
+        return Vector3D(
+            this->x * scalar,
+            this->y * scalar,
+            this->z * scalar
+        );
     }
 
-    double dotProduct(Vector3D* v)
+    double Vector3D::dotProduct(Vector3D* v)
     {
         return (
             (this->x * v->x) +
@@ -81,31 +76,31 @@ public:
         );
     }
 
-    Vector3D* crossProduct(Vector3D* v)
+    Vector3D Vector3D::crossProduct(Vector3D* v)
     {
-        return new Vector3D(
+        return Vector3D(
             (this->y * v->z) - (this->z * v->y),
             (this->x * v->z) - (this->z * v->x),
             (this->x * v->y) - (this->y * v->x)
         );
     }
 
-    double distanceBetween(Vector3D* v)
+    double Vector3D::distanceBetween(Vector3D* v)
     {
         double x = v->x - this->x;
         double y = v->y - this->y;
         double z = v->z - this->z;
-        
-        return std::sqrt(x*x + y*y + z*z);
+
+        return std::sqrt(x * x + y * y + z * z);
     }
 
-    void consoleDisplay()
+    void Vector3D::consoleDisplay()
     {
         printf("(%lf, %lf, %lf)\n", this->x, this->y, this->z);
     }
 
     // right now this thing pisses memory EVERYWHERE lol
-    static void debug(Vector3D v1, Vector3D v2)
+    void Vector3D::debug(Vector3D v1, Vector3D v2)
     {
         printf("Testing Vectors\n");
         v1.consoleDisplay();
@@ -118,25 +113,25 @@ public:
         printf("V2:\t%lf\n", v2.magnitude());
 
         printf("Adding V1 + V2\n");
-        v1.add(&v2);
-        printf("(%lf, %lf, %lf)\n", v1.x, v1.y, v1.z);
+        Vector3D sum = v1.add(&v2);
+        printf("(%lf, %lf, %lf)\n", sum.x, sum.y, sum.z);
 
         printf("Multiplying V1 * 5\n");
         v1.consoleDisplay();
         printf("\n");
-        v1.multiply(5)->consoleDisplay();
+        v1.multiply(5).consoleDisplay();
         printf("\n");
 
         printf("Normalized Magnitude (Expects 1.0):\n");
-        printf("V1:\t%lf\n", v1.normalized()->magnitude());
-        printf("V2:\t%lf\n\n", v2.normalized()->magnitude());
+        printf("V1:\t%lf\n", v1.normalized().magnitude());
+        printf("V2:\t%lf\n\n", v2.normalized().magnitude());
 
         printf("Dot Product:\n");
         printf("V1 . V2 = %lf\n\n", v1.dotProduct(&v2));
 
         printf("Cross Product:\n");
-        Vector3D* cross = v1.crossProduct(&v2);
-        cross->consoleDisplay();
+        Vector3D cross = v1.crossProduct(&v2);
+        cross.consoleDisplay();
         printf("\n");
     }
-};
+}
--- CudaTest/kernel.cu
+++ CudaTest/kernel.cu
@@ -2,13 +2,19 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
-#include "./Vector3D.cpp"
+#include "./components/headers/Ray.h"
+
+using namespace components;
 
 int main()
 {
-    Vector3D vector1(5.0, 3.0, 10.0);
-    Vector3D vector2(2.0, 7.0, 1.0);
-    Vector3D::debug(vector1, vector2);
+    Vector3D v1 = Vector3D(0, 0, 0);
+    Vector3D v2 = Vector3D(1, 2, 3);
+    Ray ray = Ray(v1, v2);
+
+    Vector3D resultant = ray.evaluate(5);
+
+    printf("Resultant:\n\tx: %lf\n\ty: %lf\n\tz: %lf", resultant.x, resultant.y, resultant.z);
 
     return 0;
 }
